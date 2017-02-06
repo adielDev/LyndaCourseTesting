f609f39c0081944fc03503e363c4e8ba995c2ff9
adiel <adielbot@outlook.com>
2017-02-06 15:29:16 +0200

    init git

---
+++ app/src/main/java/adiel/lyndacoursetesting/activities/JavaTestingActivity.java
@@ -0,0 +1,15 @@
+package adiel.lyndacoursetesting.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import adiel.lyndacoursetesting.R;
+
+public class JavaTestingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_java_testing);
+    }
+}
---
+++ app/src/main/java/adiel/lyndacoursetesting/activities/Person.java
@@ -0,0 +1,8 @@
+package adiel.lyndacoursetesting.activities;
+
+/**
+ * Created by recntrek7 on 06/02/17.
+ */
+
+public class Person {
+}
